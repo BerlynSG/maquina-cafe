32ced6bf45c52ca02eff8ae14e8d9247d6e65bdd
BerlynSG <135674997+BerlynSG@users.noreply.github.com>
2024-11-25 22:49:30 -0400

    cosas de c#

--- obj/Debug/net8.0/maquina-cafe.AssemblyInfo.cs
+++ obj/Debug/net8.0/maquina-cafe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("maquina-cafe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e2d42c2c31f52a171574284a4c10cec46bebd4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19fcf96ea8ddea7a429ae196cb931459df9876ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("maquina-cafe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("maquina-cafe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
